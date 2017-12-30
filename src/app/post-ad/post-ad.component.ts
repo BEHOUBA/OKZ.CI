c80f2c129357a4a48e21726160b213bd2eca2f47
behouba <behouba@gmail.com>
2017-12-30 04:11:26 +0300

    update of certain components

--- src/app/post-ad/post-ad.component.ts
+++ src/app/post-ad/post-ad.component.ts
@@ -11,7 +11,7 @@ export class PostAdComponent implements OnInit {
   hideElement() {
       const select = (<HTMLSelectElement>document.getElementById('category')) ;
       const selectedOption = select.options[select.selectedIndex].value;
-      const list = document.getElementById('new-post').getElementsByTagName('li') ;
+      const list = document.getElementById('form-field').getElementsByTagName('div') ;
       const elementsToHide = (document.getElementsByClassName(selectedOption));
       for (let i = 0; i < list.length; i++){
         if(list[i].classList.contains(selectedOption) || list[i].classList.contains('all')){
